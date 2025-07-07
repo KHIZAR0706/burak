1ae93bbdf98cefcf04e154a379e9210d20af41a8
KHIZAR0706 <giyoskhujanazarov@gmail.com>
2025-07-08 01:09:13 +0900

    fix: create sessions on login and signup process

--- src/controllers/restaurant.controller.ts
+++ src/controllers/restaurant.controller.ts
@@ -1,9 +1,10 @@
 import { Request, Response } from "express";
 import { T } from "../libs/types/common";
 import MemberService from "../models/Member.service";
-import { LoginInput, MemberInput } from "../libs/types/member";
+import { AdminRequest, LoginInput, MemberInput } from "../libs/types/member";
 import { MemberType } from "../libs/enums/member.enum";
 
+const memberService = new MemberService();
 
 const restaurantController: T = {};
 restaurantController.goHome = (req: Request, res: Response) => {
@@ -35,34 +36,35 @@ restaurantController.getLogin = (req: Request, res: Response) => {
 };
 
 
-restaurantController.processSignup = async (req: Request, res: Response) => {
+restaurantController.processSignup = async (req: AdminRequest, res: Response) => {
   try {
     console.log("processSignup");
-    // console.log("body:", req.body);
-
+  
     const newMember: MemberInput = req.body;
     newMember.memberType = MemberType.RESTAURANT;
-
-    const memberService = new MemberService();
     const result = await memberService.processSignup(newMember);
 
+   req.session.member = result;
+   req.session.save(function() {
     res.send(result);
+   }); 
   } catch (err) {
     console.log("Error, processSignup:", err);
     res.send(err);
   }
 };
 
-restaurantController.processLogin = async (req: Request, res: Response) => {
+restaurantController.processLogin = async (req: AdminRequest, res: Response) => {
   try {
     console.log("processLogin");
-    console.log("body:", req.body);
-    const input: LoginInput = req.body;
     
-    const memberService = new MemberService();
+    const input: LoginInput = req.body;
     const result = await memberService.processLogin(input);
-     
-    res.send(result);
+
+    req.session.member = result;
+    req.session.save(function () {
+       res.send(result);
+    });
   } catch (err) {
     console.log("Error, processLogin:", err);
     res.send(err);
