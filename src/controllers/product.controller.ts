f81584d1fcaebf39facd77a854f1a75cc09e5eb6
KHIZAR0706 <giyoskhujanazarov@gmail.com>
2025-07-12 02:51:57 +0900

    fix: modify updateChosenProduct business logic

--- src/controllers/product.controller.ts
+++ src/controllers/product.controller.ts
@@ -45,22 +45,28 @@ productController.createNewProduct = async (req: AdminRequest, res: Response) =>
         );
     } catch (err) {
         console.log("Error, createNewProduct:", err);
-        if (err instanceof Errors) res.status(err.code).json(err);
-        else res.status(Errors.standard.code).json(Errors.standard);
-    }
-};
-
-productController.updateChosenProduct = async (req: Request, res: Response) => {
-    try {
-        console.log("updateChosenProduct");
-    } catch (err) {
-        console.log("Error, updateChosenProduct:", err);
         const message =
             err instanceof Errors ? err.message : Message.SOMETHING_WENT_WRONG;
         res.send(
            
             `<script> alert("${message}"); window.location.replace('admin/product/all') </script>` 
         );
+    }
+};
+
+productController.updateChosenProduct = async (req: Request, res: Response) => {
+    try {
+        console.log("updateChosenProduct");
+        const id = req.params.id;
+        // console.log("id:", id);
+
+        const result = await productService.updateChosenProduct( id, req.body);
+
+        res.status(HttpCode.OK).json({ data: result });
+    } catch (err) {
+        console.log("Error, updateChosenProduct:", err);
+        if(err instanceof Errors) res.status(err.code).json(err);
+        else res.status(Errors.standard.code).json(Errors.standard);
     }
 };
 
