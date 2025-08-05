ba82fc4ab47f359af96375ed3164dff8a13e0a14
KHIZAR0706 <giyoskhujanazarov@gmail.com>
2025-08-05 15:14:06 +0900

    feat: develop verify authenticated user credentials

--- src/models/Auth.service.ts
+++ src/models/Auth.service.ts
@@ -4,7 +4,11 @@ import { Member } from "../libs/types/member";
 import  jwt from "jsonwebtoken";
 
 class Authservice {
-    constructor() {}
+    private readonly secretToken;
+
+    constructor() {
+       this.secretToken = process.env.SECRET_TOKEN as string;
+    }
 
     public async createToken(payload: Member) {
       return new Promise((resolve, reject) => {
@@ -21,6 +25,16 @@ class Authservice {
           }
         );
       });
+    }
+
+   
+    public async checkAuth(token: string): Promise<Member> {
+      const result = (await jwt.verify(
+        token, 
+        this.secretToken 
+      )) as Member;
+      console.log(`---- [AUTH] memberNick: ${result.memberNick} ----`);
+      return result; 
     }
  } 
 
