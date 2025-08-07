61879a1dd66e432eaf4ada4b4a417ae3f7b4def0
KHIZAR0706 <giyoskhujanazarov@gmail.com>
2025-08-07 17:22:39 +0900

    feat: create getRestaurant rest Api

--- src/controllers/member.controllers.ts
+++ src/controllers/member.controllers.ts
@@ -10,6 +10,21 @@ const memberService = new MemberService();
 const authservice = new Authservice
 
 const memberController: T = {};
+
+memberController.getRestaurant = async (req: Request, res: Response) => {
+  try {
+    console.log("getRestaurant");
+    const result = await memberService.getRestaurant();
+    console.log("result:", result);
+    
+    res.status(HttpCode.OK).json(result);
+  } catch (err) {
+    console.log("Error, getRestaurant:", err);
+    if(err instanceof Errors) res.status(err.code).json(err);
+    else res.status(Errors.standard.code).json(Errors.standard);   
+  }
+};
+ 
 memberController.signup = async (req: Request, res: Response) => {
   try {
     console.log("signup");
@@ -96,7 +111,7 @@ memberController.getTopUsers = async (req: Request, res: Response) => {
     console.log("getTopUsers");
     const result = await memberService.getTopUsers();
     console.log("result:", result);
-    
+
     res.status(HttpCode.OK).json(result);
   } catch (err) {
     console.log("Error, getTopUsers:", err);
